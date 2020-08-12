a06f87ebd782610aa33fdbca6b40ff4959414d56
mrjoshuamartinez <mrjoshuamartinez@gmail.com>
2020-08-12 19:24:24 -0400

    added lines for stretch goals

--- components/Menu.js
+++ components/Menu.js
@@ -35,11 +35,23 @@ function menuMaker(array){
 
   //   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
   const menuButton = document.querySelector('.menu-button');
+  const hideMenu = document.querySelector('.articles');
+  let menuOpen = false;
 
   //   Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
   menuButton.addEventListener('click',() => {
     menu.classList.toggle('menu--open');
+    menuOpen = true;
   })
+
+  hideMenu.addEventListener('click', () => {
+    if (menuOpen === false) {
+      console.log('Menu is closed already!')
+    } else if (menuOpen === true) {
+      menuOpen = true;
+      menu.classList.toggle('menu--open');
+    }
+  });
 
   //   Step 5: return your div with a 'menu' class.
   return menu;
